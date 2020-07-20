4c133631f087d8f3aa34b9da629fd95e0ae9bfcd
公司 <gxp>
2020-07-20 10:52:58 +0800

    Initial commit

---
+++ xutils_library/src/main/java/org/xutils/common/util/FileUtil.java
@@ -0,0 +1,213 @@
+package org.xutils.common.util;
+
+import android.content.Context;
+import android.os.Environment;
+import android.os.StatFs;
+
+import org.xutils.x;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.math.BigDecimal;
+
+public class FileUtil {
+
+    private FileUtil() {
+    }
+
+    public static File getCacheDir(String dirName) {
+        File result;
+        if (existsSdcard()) {
+            File cacheDir = x.app().getExternalCacheDir();
+            if (cacheDir == null) {
+                result = new File(Environment.getExternalStorageDirectory(),
+                        "Android/data/" + x.app().getPackageName() + "/cache/" + dirName);
+            } else {
+                result = new File(cacheDir, dirName);
+            }
+        } else {
+            result = new File(x.app().getCacheDir(), dirName);
+        }
+        if (result.exists() || result.mkdirs()) {
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 检查磁盘空间是否大于10mb
+     *
+     * @return true 大于
+     */
+    public static boolean isDiskAvailable() {
+        long size = getDiskAvailableSize();
+        return size > 10 * 1024 * 1024; // > 10bm
+    }
+
+    /**
+     * 获取磁盘可用空间
+     *
+     * @return byte 单位 kb
+     */
+    public static long getDiskAvailableSize() {
+        if (!existsSdcard()) return 0;
+        File path = Environment.getExternalStorageDirectory(); // 取得sdcard文件路径
+        StatFs stat = new StatFs(path.getAbsolutePath());
+        long blockSize = stat.getBlockSize();
+        long availableBlocks = stat.getAvailableBlocks();
+        return availableBlocks * blockSize;
+        // (availableBlocks * blockSize)/1024 KIB 单位
+        // (availableBlocks * blockSize)/1024 /1024 MIB单位
+    }
+
+    public static Boolean existsSdcard() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+    }
+
+    public static long getFileOrDirSize(File file) {
+        if (!file.exists()) return 0;
+        if (!file.isDirectory()) return file.length();
+
+        long length = 0;
+        File[] list = file.listFiles();
+        if (list != null) { // 文件夹被删除时, 子文件正在被写入, 文件属性异常返回null.
+            for (File item : list) {
+                length += getFileOrDirSize(item);
+            }
+        }
+
+        return length;
+    }
+
+    /**
+     * 复制文件到指定文件
+     *
+     * @param fromPath 源文件
+     * @param toPath   复制到的文件
+     * @return true 成功，false 失败
+     */
+    public static boolean copy(String fromPath, String toPath) {
+        boolean result = false;
+        File from = new File(fromPath);
+        if (!from.exists()) {
+            return result;
+        }
+
+        File toFile = new File(toPath);
+        IOUtil.deleteFileOrDir(toFile);
+        File toDir = toFile.getParentFile();
+        if (toDir.exists() || toDir.mkdirs()) {
+            FileInputStream in = null;
+            FileOutputStream out = null;
+            try {
+                in = new FileInputStream(from);
+                out = new FileOutputStream(toFile);
+                IOUtil.copy(in, out);
+                result = true;
+            } catch (Throwable ex) {
+                LogUtil.d(ex.getMessage(), ex);
+                result = false;
+            } finally {
+                IOUtil.closeQuietly(in);
+                IOUtil.closeQuietly(out);
+            }
+        }
+        return result;
+    }
+
+    /**
+     *  删除文件
+     */
+    private static boolean deleteFilesByDirectory(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                boolean success = deleteFilesByDirectory(new File(dir, children[i]));
+                if (!success) {
+                    return false;
+                }
+            }
+        }
+        return dir.delete();
+    }
+
+    /**
+     * * 清除本应用内部缓存(/data/data/com.xxx.xxx/cache) * *
+     *
+     * @param context
+     */
+    public static boolean cleanInternalCache(Context context) {
+        return deleteFilesByDirectory(context.getCacheDir());
+    }
+
+    /**
+     * * 清除外部cache下的内容(/mnt/sdcard/android/data/com.xxx.xxx/cache)
+     *
+     * @param context
+     */
+    public static void cleanExternalCache(Context context) {
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            deleteFilesByDirectory(context.getExternalCacheDir());
+        }
+    }
+
+    public static String getCacheSize(File file) throws Exception {
+        return getFormatSize(getFolderSize(file));
+    }
+
+    // 获取文件
+    // Context.getExternalFilesDir() --> SDCard/Android/data/你的应用的包名/files/目录，一般放一些长时间保存的数据
+    // Context.getExternalCacheDir() --> SDCard/Android/data/你的应用包名/cache/目录，一般存放临时缓存数据
+    public static long getFolderSize(File file) throws Exception {
+        long size = 0;
+        try {
+            File[] fileList = file.listFiles();
+            for (int i = 0; i < fileList.length; i++) {
+                // 如果下面还有文件
+                if (fileList[i].isDirectory()) {
+                    size = size + getFolderSize(fileList[i]);
+                } else {
+                    size = size + fileList[i].length();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
+
+    /**
+     * 格式化单位
+     *
+     * @param size
+     * @return
+     */
+    public static String getFormatSize(double size) {
+        double kiloByte = size / 1024;
+
+
+        double megaByte = kiloByte / 1024;
+        if (megaByte < 1) {
+            BigDecimal result1 = new BigDecimal(Double.toString(kiloByte));
+            return result1.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "KB";
+        }
+
+        double gigaByte = megaByte / 1024;
+        if (gigaByte < 1) {
+            BigDecimal result2 = new BigDecimal(Double.toString(megaByte));
+            return result2.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "MB";
+        }
+
+        double teraBytes = gigaByte / 1024;
+        if (teraBytes < 1) {
+            BigDecimal result3 = new BigDecimal(Double.toString(gigaByte));
+            return result3.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "GB";
+        }
+        BigDecimal result4 = new BigDecimal(teraBytes);
+        return result4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "TB";
+    }
+
+
+}
